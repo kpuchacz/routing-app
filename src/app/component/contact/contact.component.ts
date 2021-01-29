54c8c5442dbd5384d0670315e2776f73f2cac9a9
kpuchacz <71168561+kpuchacz@users.noreply.github.com>
2021-01-29 15:00:01 +0100

    No changes in bootstrap css was made. Fixed card overlay; About Section with drop-menu (still to be fixed - the color)

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -1,46 +1,15 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit, AfterViewInit {
+export class ContactComponent implements OnInit {
 
   constructor() {
   }
 
-  @ViewChild('cardToOverlay', {static: true})
-  cardToOverlay: ElementRef;
-
   ngOnInit(): void {
   }
-
-  // tslint:disable-next-line:typedef
-  ngAfterViewInit() {
-    console.log('afterinit');
-    setTimeout(() => {
-      console.log(this.cardToOverlay.nativeElement.offsetHeight);
-    }, 200);
-    // tslint:disable-next-line:prefer-const
-    let height = this.cardToOverlay.nativeElement.offsetHeight;
-
-    console.log('Height: ' + height);
-  }
 }
-
-// export class AppComponent implements AfterViewInit {
-//   constructor() {
-//   }
-//
-//   @ViewChild('myIdentifier')
-//   myIdentifier: ElementRef;
-//
-//   // tslint:disable-next-line:typedef
-//   ngAfterViewInit() {
-//     // tslint:disable-next-line:prefer-const
-//     let height = this.myIdentifier.nativeElement.offsetHeight;
-//
-//     console.log('Height: ' + height);
-//   }
-// }
